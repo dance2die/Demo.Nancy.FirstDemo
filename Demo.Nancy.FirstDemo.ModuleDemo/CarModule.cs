d113353e20f529231d6b2f5dbe33e05bbab2ffef
dance2die <dontbothermewithspam@gmail.com>
2016-09-15 06:30:02 -0400

    NancyModule returns JSON data

--- Demo.Nancy.FirstDemo.ModuleDemo/CarModule.cs
+++ Demo.Nancy.FirstDemo.ModuleDemo/CarModule.cs
@@ -7,6 +7,29 @@ namespace Demo.Nancy.FirstDemo.ModuleDemo
 		public CarModule()
 		{
 			Get["/status"] = _ => "Hello world!";
+			Get["/car/{id}"] = parameters =>
+			{
+				int id = parameters.id;
+
+				//return Negotiate
+				//	.WithStatusCode(HttpStatusCode.OK)
+				//	.WithModel(id);
+				////.WithView("CarView");
+
+				var test = new
+				{
+					ID = id,
+					Name = "Sam Meister",
+					Twitter = "@dance2die",
+					Occupation = "Software Engineer"
+				};
+
+				return Response
+					//.AsJson(id)
+					.AsJson(test)
+					.WithStatusCode(HttpStatusCode.OK);
+			};
+
 		}
 	}
 }
