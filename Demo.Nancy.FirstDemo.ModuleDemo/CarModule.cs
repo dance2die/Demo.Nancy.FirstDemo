4e9c65875a69c5c6ad28837d9e9fa650188aac27
dance2die <dontbothermewithspam@gmail.com>
2016-09-14 22:05:28 -0400

    Created a first successful Nancy module

---
+++ Demo.Nancy.FirstDemo.ModuleDemo/CarModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Demo.Nancy.FirstDemo.ModuleDemo
+{
+	public class CarModule : NancyModule
+	{
+		public CarModule()
+		{
+			Get["/status"] = _ => "Hello world!";
+		}
+	}
+}
---
+++ Demo.Nancy.FirstDemo.ModuleDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Demo.Nancy.FirstDemo.ModuleDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Demo.Nancy.FirstDemo.ModuleDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d2e2a2f1-bd5e-4310-8392-064fd38a3339")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
